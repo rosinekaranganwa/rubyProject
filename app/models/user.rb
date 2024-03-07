3f52a5f750ad736dd071f1291f2114c60c890ace
rosinekaranganwa <rosinekaranganwa@gmail.com>
2024-03-07 18:25:46 -0500

    added static pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-    has_many:microposts
-    validates name, presence: true
+    has_many :microposts
+    validates :name, presence: true
+    validates :email, presence: true
+
 end
